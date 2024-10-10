1b2a3e2f5baee7cace40d1ea8e3358fe05c14098
Tanisha <vermatanisha620@gmail.com>
2024-10-10 17:25:23 +0530

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,24 @@
+// TEMPRATURE CONVERSION PROGRAM
+
+const textbox = document.getElementById("textbox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const tocelsius = document.getElementById("tocelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert(){
+         
+    if(toFahrenheit.checked){
+            temp = Number (textbox.value);
+            temp = temp* 9 / 5 + 32;
+            result.textContent = temp.toFixed(1) + "°F";
+    }
+    else if(tocelsius.checked){
+        temp = Number (textbox.value);
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = "select a unit";
+    }
+}
